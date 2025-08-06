a6241c901d017569e200809e4ab1f524cb2ce2cb
Aman kumar singh <157352533+aman-singh0409@users.noreply.github.com>
2025-08-06 11:51:01 +0530

    Create 2888-MinimumIndexOfAValidSplit

---
+++ 2888-MinimumIndexOfAValidSplit/2888-MinimumIndexOfAValidSplit.java
@@ -0,0 +1,16 @@
+// Last updated: 8/6/2025, 11:51:01 AM
+class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        Map<Integer, Integer> mpp = new HashMap<>(), left = new HashMap<>();
+        for (int i : nums) mpp.put(i, mpp.getOrDefault(i, 0) + 1);
+        
+        for (int i = 0; i < nums.size() - 1; i++) {
+            left.put(nums.get(i), left.getOrDefault(nums.get(i), 0) + 1);
+            mpp.put(nums.get(i), mpp.get(nums.get(i)) - 1);
+            
+            if (left.get(nums.get(i)) * 2 > (i + 1) && mpp.get(nums.get(i)) * 2 > (nums.size() - i - 1))
+                return i;
+        }
+        return -1;
+    }
+}
