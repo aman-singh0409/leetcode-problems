9ba1d500114ee0a7119bf3e66064b26ce01b479d
Aman kumar singh <157352533+aman-singh0409@users.noreply.github.com>
2025-08-06 11:51:35 +0530

    Create 1993-SumOfAllSubsetXorTotals

---
+++ 1993-SumOfAllSubsetXorTotals/1993-SumOfAllSubsetXorTotals.java
@@ -0,0 +1,16 @@
+// Last updated: 8/6/2025, 11:51:36 AM
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int n = nums.length, total = 0;
+        for (int mask = 0; mask < (1 << n); mask++) {
+            int subsetXor = 0;
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    subsetXor ^= nums[i];
+                }
+            }
+            total += subsetXor;
+        }
+        return total;
+    }
+}
