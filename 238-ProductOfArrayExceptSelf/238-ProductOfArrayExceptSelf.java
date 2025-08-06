e6b4e81e9c45f5964e64695fa92b269fddce8978
Aman kumar singh <157352533+aman-singh0409@users.noreply.github.com>
2025-08-06 11:52:43 +0530

    Create 238-ProductOfArrayExceptSelf

---
+++ 238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.java
@@ -0,0 +1,21 @@
+// Last updated: 8/6/2025, 11:52:44 AM
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int output[]= new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            output[i]=1;
+        }
+        int left=1;
+        for(int i=0;i<nums.length;i++){
+            output[i]*=left;
+            left*=nums[i];
+        }
+        int right=1;
+        for(int i=nums.length-1;i>=0;i--){
+            output[i]*=right;
+            right*=nums[i];
+        }
+
+        return output;
+    }
+}
