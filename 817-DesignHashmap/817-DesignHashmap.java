c90bb3763b8ccb000b932bbab227c07128978980
Aman kumar singh <157352533+aman-singh0409@users.noreply.github.com>
2025-08-06 11:52:11 +0530

    Create 817-DesignHashmap

---
+++ 817-DesignHashmap/817-DesignHashmap.java
@@ -0,0 +1,28 @@
+// Last updated: 8/6/2025, 11:52:11 AM
+class MyHashMap {
+    private int[] map;
+    public MyHashMap() {
+        map=new int[1000001];
+        Arrays.fill(map,-1);
+    }
+    
+    public void put(int key, int value) {
+        map[key]=value;
+    }
+    
+    public int get(int key) {
+        return map[key];
+    }
+    
+    public void remove(int key) {
+        map[key]=-1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
