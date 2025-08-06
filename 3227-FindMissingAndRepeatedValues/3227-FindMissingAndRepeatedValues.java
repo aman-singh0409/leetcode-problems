9ae5cbe4c204792237aad21f626564f900b4d28e
Aman kumar singh <157352533+aman-singh0409@users.noreply.github.com>
2025-08-06 11:50:55 +0530

    Create 3227-FindMissingAndRepeatedValues

---
+++ 3227-FindMissingAndRepeatedValues/3227-FindMissingAndRepeatedValues.java
@@ -0,0 +1,23 @@
+// Last updated: 8/6/2025, 11:50:55 AM
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n= grid.length;
+        int size=n*n;
+        int freq[]= new int[size+1];
+        int repeated=-1, missing =-1;
+        for(int row[]: grid){
+            for(int num: row){
+                freq[num]++;
+            }
+        }
+        for(int num=1;num<=size;num++){
+            if(freq[num]==2){
+                repeated=num;
+            }
+            if(freq[num]==0){
+                missing=num;
+            }
+        }
+        return new int[]{repeated, missing};
+    }
+}
